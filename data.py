eb879bdbe73b7638702ec4dbb006cdc10c0f4296
Pablo Arango <pabloaran17@gmail.com>
2014-11-15 21:53:24 -0500

    correct data

--- data.py
+++ data.py
@@ -9,10 +9,10 @@ while N != 2014:
 	response = urllib2.urlopen('http://data.unhcr.org/api/stats/persons_of_concern.json?year='+str(N)+'&country_of_origin=COL&country_of_residence=COL')
 	data = json.load(response)
 	data = data[0]
-	idps[str(N)] = data["total_population"]
+	idps[N] = int(data["total_population"])
 	N+=1
 	
-jsondata=json.dumps(idps, sort_keys=True, indent=4, separators=(',', ': '))
+jsondata=json.dumps([{'year': k, 'population': v} for k,v in idps.items()], indent=4, separators=(',', ': '))
 print jsondata
 
 f = open('data.json', 'w')
