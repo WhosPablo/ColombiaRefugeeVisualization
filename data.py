5fd6584851422743b617fff0303d51fba112bb7e
Pablo Arango <pabloaran17@gmail.com>
2014-11-15 23:09:44 -0500

    data

--- data.py
+++ data.py
@@ -2,19 +2,21 @@ import urllib2
 import json
 
 
-info = {}
+
 years = {}
 
 year=2000
 while year != 2014:
 	response = urllib2.urlopen('http://data.unhcr.org/api/stats/persons_of_concern.json?year='+str(year)+'&country_of_origin=COL')
 	data = json.load(response)
+	info = {}
 	info["year"] = year
 	info["total_population"] =0
 	info["refugees"] =0
 	info["returned_refugees"] = 0
 	info["idps"] = 0
 	info["asylum_seekers"] = 0
+	
 	for i in range (len(data)):
 		if data[i]["total_population"] is not None and data[i]["total_population"]!='*':
 			info["total_population"] += int(data[i]["total_population"])
@@ -27,6 +29,7 @@ while year != 2014:
 		if data[i]["asylum_seekers"] is not None:
 			info["asylum_seekers"] += int(data[i]["asylum_seekers"])
 	years[year]= info
+	print info
 	print year
 	year+=1
 print years
